0a4d09f75fffe0bebb9afe387d652392da0c337d
Asapanna Rakesh <rakeshark22@gmail.com>
2021-03-27 20:36:00 +0530

    unwanted code removed

--- js/script.js
+++ js/script.js
@@ -5,36 +5,8 @@
 		handle: 'Tiger Mask',
 		url: 'https://crejo.fun',
 		entry: './assets/tiger-jaw-mask.png',
-		background: 'hsl(35, 60%, 50%)',
+		background: 'hsl(35, 60 %, 50 %)', // bg color when only mask is displayed. 
 	}];
-	/*
-	[{
-		handle: 'Chhau Mask',
-		url: 'https://en.wikipedia.org/wiki/Chhau_mask',
-		entry: './assets/chhau-mask.jpg',
-		background: 'hsl(35, 60%, 50%)'
-	}, {
-		handle: 'Cheriyal Mask',
-		url: 'https://en.wikipedia.org/wiki/Cheriyal_scroll_painting#The_Tradition',
-		entry: './assets/cherial-woman-mask.jpg',
-		background: 'hsl(0, 40%, 15%)'
-	}, {
-		handle: 'Cheriyal Mask',
-		url: 'https://en.wikipedia.org/wiki/Cheriyal_scroll_painting#The_Tradition',
-		entry: './assets/cherial-mask.jpg',
-		background: 'hsl(198, 5%, 50%)'
-	}, {
-		handle: 'Jamini Roy',
-		url: 'https://en.wikipedia.org/wiki/Jamini_Roy#Style',
-		entry: './assets/jamini-mask.jpg',
-		background: 'radial-gradient(hsl(243, 40%, 26%), hsl(243, 60%, 17%))'
-	}, {
-		handle: 'Jamini Roy',
-		url: 'https://en.wikipedia.org/wiki/Jamini_Roy#Style',
-		entry: './assets/jamini-mother-mask.jpg',
-		background: 'radial-gradient(rgb(132, 0, 0), rgb(87, 0, 0))'
-	}];
-	*/
 
 	var videoFormats = ['mov', 'm4v', 'mp4'];
 	// var imageFormats = ['png', 'jpg'];
@@ -43,6 +15,8 @@
 	for (var i = 0; i < entries.length; i++) {
 		var obj = entries[i];
 		var el;
+		//console.log(videoFormats.indexOf(obj.entry.split('.')[2]), obj.entry.split('.')[2])
+		//-1, "png"
 		if (videoFormats.indexOf(obj.entry.split('.')[2]) > -1) {
 			el = document.createElement('video');
 			el.setAttribute('playsinline', true);
@@ -65,29 +39,6 @@
 		carousel.appendChild(el);
 	}
 
-	var flkty = new Flickity(carousel, {
-		// hash: true,
-		dragThreshold: 1,
-		selectedAttraction: 0.2,
-		friction: 0.8,
-		wrapAround: true
-	});
-
-	function updateTexture(index) {
-		var url = entries[index].entry;
-		var isVideo = videoFormats.indexOf(url.split('.')[2]) > -1;
-		faceCanvas.updateTexture(url, isVideo);
-		background.style.background = entries[index].background;
-		if ('url' in entries[index]) {
-			artist.style.pointerEvents = 'all';
-			artist.href = entries[index].url;
-		} else {
-			artist.style.pointerEvents = 'none';
-			artist.href = '#';
-		}
-		artist.textContent = entries[index].handle;
-	}
-	flkty.on('change', updateTexture);
 	const toggleBtn = document.querySelector('#visibilityToggle');
 	const toggleBtnLabel = document.querySelector('#visibilityToggle > span');
 
@@ -105,25 +56,24 @@
 	let model, faceCanvas, w, h;
 	const loaderMsg = document.querySelector('#loaderMsg');
 
-	var artist = document.querySelector('#artist');
 
 	async function renderPredictions(t) {
-    requestAnimationFrame(renderPredictions);
-		loaderMsg.textContent = 'Search face';
+		requestAnimationFrame(renderPredictions);
 		const predictions = await model.estimateFaces(webcam);
 
 		if (predictions.length > 0) {
 			const positionBufferData = predictions[0].scaledMesh.reduce((acc, pos) => acc.concat(pos), []);
+			// console.log('[positionBufferData]', positionBufferData) 
+			// flattened `predictions.scaledMesh` of len (468keypoints * 3coords) = `1404`
+
 			if (!faceCanvas) {
 				const props = {
-					id: 'faceCanvas',
-					textureFilePath: entries[0].entry,
+					id: 'faceCanvas', // `id` of `canvas element`
+					textureFilePath: entries[0].entry, // image to be displayed
 					w,
 					h
 				}
 				faceCanvas = new FacePaint(props);
-				updateTexture(flkty.selectedIndex);
-				document.querySelector('#loader').style.display = 'none';
 				return;
 			}
 			faceCanvas.render(positionBufferData);
@@ -131,12 +81,12 @@
 	}
 	async function main() {
 		try {
-			loaderMsg.textContent = 'Load webcam';
+			//loaderMsg.textContent = 'Load webcam';
 			const stream = await navigator.mediaDevices.getUserMedia({
 				video: true,
 				audio: false
 			});
-			webcam.srcObject = stream;
+			webcam.srcObject = stream; // `video` tag
 			await new Promise(function (res) {
 				webcam.onloadedmetadata = function () {
 					w = webcam.videoWidth;
@@ -151,7 +101,7 @@
 			webcam.setAttribute('muted', true);
 			webcam.setAttribute('playsinline', true);
 			webcam.play();
-			loaderMsg.textContent = 'Load model';
+			//loaderMsg.textContent = 'Load model';
 			// Load the MediaPipe facemesh model.
 			model = await facemesh.load({
 				maxContinuousChecks: 5,
@@ -160,8 +110,10 @@
 				iouThreshold: 0.3,
 				scoreThreshold: 0.75
 			});
-			loaderMsg.textContent = 'Load media';
-			await Promise.all(assets);
+
+			//loaderMsg.textContent = 'Load media';
+			// await Promise.all(assets);
+
 			renderPredictions();
 		} catch (e) {
 			alert(e);
